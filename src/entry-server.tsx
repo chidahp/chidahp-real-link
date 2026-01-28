3ca6bddeded3557c436053892908e2a1e0098540
topspinppy <tl.topspin.48@gmail.com>
2026-01-28 22:21:25 +0700

    feat: add Facebook domain verification meta tag to enhance site integration

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -10,6 +10,7 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
           <title>สำนักพิมพ์ชี้ดาบ - chidahp</title>
+          <meta name="facebook-domain-verification" content="bcjvgx71le5tk1mlh1x1jchd8cfyla" />
           {/* Google AdSense */}
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360416910031647" crossorigin="anonymous"></script>
 
