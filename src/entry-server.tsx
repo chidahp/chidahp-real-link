30eedc4729a1567661775b399b155b47f3d88b4d
topspinppy <tl.topspin.48@gmail.com>
2026-03-24 17:14:39 +0700

    feat: add Google Analytics integration for improved tracking and analytics capabilities

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -13,7 +13,18 @@ export default createHandler(() => (
           <meta name="facebook-domain-verification" content="bcjvgx71le5tk1mlh1x1jchd8cfyla" />
           {/* Google AdSense */}
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360416910031647" crossorigin="anonymous"></script>
-
+          
+          {/* Google analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-SNH03HJL2B"
+          ></script>
+          <script>{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SNH03HJL2B');
+          `}</script>
           {assets}
         </head>
         <body>
